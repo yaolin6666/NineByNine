f4c933824702e5adc508737678b89f1ac400cd1b
yaolin6666 <37007707+yaolin6666@users.noreply.github.com>
2023-04-07 13:29:12 +0800

    Update NineByNine.cpp

--- NineByNine.cpp
+++ NineByNine.cpp
@@ -1,2 +1,2 @@
 #include<cstdio>
-int main(){for(int i=0;i++<9;printf("\n"))for(int j=0;j++<i;)printf("%d×%d=%d\t",j,i,j*i);}
+int main(){for(int i=0;i++<9;puts(""))for(int j=0;j++<i;)printf("%d×%d=%d\t",j,i,j*i);}
